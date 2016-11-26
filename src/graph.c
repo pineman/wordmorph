2da48aba9d054c6152dc91fc7a8172afc8fe7157
joajfreitas <joaj.freitas@gmail.com>
2016-11-26 00:58:02 +0000

    lista de adjacencias, done

--- src/graph.c
+++ src/graph.c
@@ -9,46 +9,132 @@ struct _Node {
 };
 
 struct _Link {
-    int weight;
-    int index;
+    unsigned int weight;
+    unsigned int index;
 };
 
 struct _Graph {
     Node **nodes;
-    int free;
-    int size;
+    unsigned int free;
+    unsigned int size;
+    unsigned int max_weight;
 };
 
-Graph *g_init(unsigned int size)
+Graph *g_init(unsigned int size, unsigned int max_weight)
 {
-    Graph *graph = (Graph *) emalloc(sizeof(Graph));
-    graph->free = size;
-    graph->size = size;
+    Graph *g = (Graph *) emalloc(sizeof(Graph));
+    g->nodes = (Node **) malloc(sizeof(Node *) * size);
+    g->free = 0;
+    g->size = size;
+    g->max_weight = max_weight;
 
-    return graph;
+    return g;
 }
 
-void g_insert(Graph *graph, Item item)
+void g_insert(Graph *g, Item i)
 {
-    Node *new_node = _init_node(item);
-    graph->nodes[graph->free] = new_node; 
-    (graph->free)--;
+    Node *new_node;
+    if (g->free == g->size) {
+        puts("Erro: grafo esta cheio");
+        exit(EXIT_FAILURE);
+    }
+    new_node = _init_node(i);
+    g->nodes[g->free] = new_node; 
+    g->free++;
 }
 
-void g_free(Graph *graph)
+void g_free(Graph *g)
 {
-    int i;
+    unsigned int i;
     Node *aux;
-    for (i=0; i<graph->size; i++) {
-        aux = graph->nodes[i];
+    for (i=0; i<g->size; i++) {
+        aux = g->nodes[i];
         l_free(aux->adj, free);
         free(aux);
     }
 
-    free(graph->nodes);
-    free(graph);
+    free(g->nodes);
+    free(g);
 
     return;
+}
+
+Node *g_get_node(Graph *g, unsigned int index)
+{
+    if (g == NULL) {
+        fprintf(stderr, "Erro: grafo não existe!");
+        exit(EXIT_FAILURE);
+    }
+    else if (index > g->size) {
+        fprintf(stderr, "Erro: index está fora dos limites do grafo!");
+        exit(EXIT_FAILURE);
+    }
+
+    return g->nodes[index];
+}
+
+Item g_get_item(Node *n)
+{
+    if (n == NULL) {
+        fprintf(stderr, "Erro: nó não existe!");
+        exit(EXIT_FAILURE);
+    }
+    else if (n->item == NULL) {
+        fprintf(stderr, "Erro: item não existe!");
+        exit(EXIT_FAILURE);
+    }
+
+    return n->item;
+}
+
+List *g_get_adj(Node *n)
+{
+    if (n == NULL)
+        puts("Error: lista não inicializada");
+
+    return n->adj;
+}
+
+unsigned int g_get_weight(Link *l)
+{
+    return l->weight;
+}
+
+unsigned int g_get_index(Link *l)
+{
+    return l->index;
+}
+
+void g_update_links(Graph *g, unsigned int (*calc_weight)(Item i1, Item i2, 
+                                                          unsigned int max))
+{
+    unsigned int i, j;
+    unsigned int weight;
+    for (i=0; i<g->free; i++)
+        for (j=0; j<i; j++) {
+            if ((weight = calc_weight(g->nodes[i]->item, g->nodes[j]->item, g->max_weight)) <= g->max_weight)
+                _make_link(g, i, j, weight);
+        }
+
+}
+
+void _make_link (Graph *g, unsigned int i1, unsigned int i2, 
+                    unsigned int weight) 
+{
+    Link *l1 = _init_link(i2, weight);
+    Link *l2 = _init_link(i1, weight);
+
+    l_insert(&(g->nodes[i1]->adj), l1);
+    l_insert(&(g->nodes[i2]->adj), l2);   
+}
+
+Link *_init_link(unsigned int index, unsigned int weight)
+{
+    Link *new_link = (Link *) malloc(sizeof(Link));
+    new_link->index = index;
+    new_link->weight = weight;
+
+    return new_link;
 }
 
 bool g_cmp_links(Link *l1, Link *l2)
@@ -56,10 +142,10 @@ bool g_cmp_links(Link *l1, Link *l2)
     return (l1->weight > l2->weight);
 }
 
-Node *_init_node(Item item)
+Node *_init_node(Item i)
 {
     Node *new_node = (Node *) ecalloc(1, sizeof(Node));
-    new_node->item = item;
+    new_node->item = i;
     new_node->adj = l_init();
 
     return new_node;
--- src/main.c
+++ src/main.c
@@ -3,22 +3,46 @@
 
 #include "list.h"
 #include "heap.h"
+#include "graph.h"
 
 int main(int argc, char **argv) 
 {
-    int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int i,j;
-    h_init(10);
+    unsigned int i;
+    int **pointers = (int **) malloc(sizeof(int *) * 10);
+    Graph *graph = g_init(10, 2);
+    
+
     for (i=0; i<10; i++) {
-        h_insert(array[i]);
+        pointers[i] = (int *) malloc(sizeof(int));
+        *(pointers[i]) = i;
+    } 
+
+    for (i=0; i<10; i++) {
+        g_insert(graph, pointers[i]);
     }
 
+    
+    printf("Nós:\n");
+    for (i=0; i<10; i++) {
+        printf("%d\n", *((int *) (g_get_item(g_get_node(graph, i)))));
+    }
 
-    for (j=0; j<10; j++) {
-        printf("%d\n", h_delmax());
+    g_update_links(graph, calc_weight);
+
+    printf("Adj\n:");
+    for (i=0; i<10; i++){
+        List *aux = g_get_adj(g_get_node(graph, i));
+        printf("Nó %d:\n", i);
+        while(aux != NULL) {
+            printf("Link: %d, %d\n", g_get_index(l_get_item(aux)), g_get_weight(l_get_item(aux)));
+            aux = l_get_next(aux);
+        }
+
     }
 
 
 
 	return EXIT_SUCCESS;
 }
+
+
